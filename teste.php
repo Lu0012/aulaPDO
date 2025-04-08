b53df3906a29a815970a93bd3df0be78f8f378ea
Lu0012 <caiosoaresmm@gmail.com>
2025-04-08 20:38:34 -0300

    atualizado

--- teste.php
+++ teste.php
@@ -15,11 +15,13 @@
         try {
             $conexao = new PDO("mysql:host=$servidor;dbname=$db", $usuario, $senha);
             $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexão realizada com sucesso";
+        
         } 
         catch (PDOException $e) {
-
+        echo "Não foi possível conectar <br>" . $e ->getMessage();
         }
-        
+        $conexao = null;
     ?>
 
 </body>
